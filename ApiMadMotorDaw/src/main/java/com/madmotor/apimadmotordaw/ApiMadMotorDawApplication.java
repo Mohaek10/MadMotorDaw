bb33f013bba7ad8d8ccff25025c5ad31ec9eae60
Mohaek <elkasmimoha@gmail.com>
2023-11-15 12:32:11 +0100

    Init SpringBoot

---
+++ ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/ApiMadMotorDawApplication.java
@@ -0,0 +1,13 @@
+package com.madmotor.apimadmotordaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMadMotorDawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMadMotorDawApplication.class, args);
+    }
+
+}
