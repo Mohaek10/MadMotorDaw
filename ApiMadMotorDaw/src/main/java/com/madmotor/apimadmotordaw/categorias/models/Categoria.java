a6d935022eb83e98c2db0610c7735e337b985863
Mohaek <elkasmimoha@gmail.com>
2023-11-15 13:14:34 +0100

    Clase Categoria

--- ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/categorias/models/Categoria.java
+++ ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/categorias/models/Categoria.java
@@ -1,4 +1,45 @@
 package com.madmotor.apimadmotordaw.categorias.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+@Builder
+@Entity
+@Table(name = "CATEGORIAS")
+@EntityListeners(AuditingEntityListener.class)
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Builder.Default
+    private UUID id=UUID.randomUUID();
+
+    @Column(name = "nombre", nullable = false)
+    @Length(min=3, message = "El nombre debe tener al menos 3 caracteres")
+    private String name;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Builder.Default
+    private LocalDateTime createdAt=LocalDateTime.now();
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Builder.Default
+    private LocalDateTime updatedAt=LocalDateTime.now();
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "boolean default false")
+    @Builder.Default
+    private Boolean isDeleted=false;
+
 }
--- ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/vehiculos/models/Vehiculo.java
+++ ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/vehiculos/models/Vehiculo.java
@@ -28,24 +28,29 @@ public class Vehiculo {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id=UUID.randomUUID();
 
+    @Column(name = "marca", nullable = false)
     @NotBlank(message = "Debe de tener una marca")
     private String marca;
 
+    @Column(name = "modelo", nullable = false)
     @NotBlank(message = "El modelo no puede estar vacio")
     private String modelo;
 
+    @Column(name = "precio", nullable = false)
     @Min(value = 0, message = "El precio no puede ser negativo")
     @Builder.Default
     private Double precio=0.0;
 
+    @Column(name = "stock", nullable = false)
     @Min(value =0, message = "El stock no puede ser negativo")
     @Builder.Default
     private Integer stock=0;
 
-    @Column (columnDefinition="TEXT DEFAULT '"+IMAGE_DEFAULT+"'")
+    @Column (name="imagen",columnDefinition="TEXT DEFAULT '"+IMAGE_DEFAULT+"'")
     @Builder.Default
     private String imagen=IMAGE_DEFAULT;
 
+    @Column(name = "descripcion", nullable = false)
     @NotBlank(message = "La descripcion no puede estar vacia")
     private String descripcion;
 
