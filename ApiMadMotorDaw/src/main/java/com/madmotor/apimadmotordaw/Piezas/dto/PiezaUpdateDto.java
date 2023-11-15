e2a908eb76ceaee208ac84613c465217a300f69f
Ruben Fernandez <rubenfernandezp03@gmail.com>
2023-11-15 13:02:34 +0100

    Created PiezasDto classes and Piezas Repository

---
+++ ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/Piezas/dto/PiezaUpdateDto.java
@@ -0,0 +1,5 @@
+package com.madmotor.apimadmotordaw.Piezas.dto;
+
+public class PiezaUpdateDto {
+
+}
---
+++ ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/Piezas/models/Pieza.java
@@ -0,0 +1,5 @@
+package com.madmotor.apimadmotordaw.Piezas.models;
+
+public class Pieza {
+
+}
