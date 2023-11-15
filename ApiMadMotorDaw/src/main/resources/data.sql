60f498dc3e7597686fd1779fd3c566cc042734bb
Ruben Fernandez <rubenfernandezp03@gmail.com>
2023-11-15 14:53:05 +0100

    Controller and PageResponse added and data.sql

---
+++ ApiMadMotorDaw/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO PIEZAS (id, nombre, descripcion, precio, stock, image)
+VALUES
+    (UUID(), 'Pieza 1', 'Descripción de la pieza 1', 19.99, 10, 'https://via.placeholder.com/150'),
+    (UUID(), 'Pieza 2', 'Descripción de la pieza 2', 29.99, 5, 'https://via.placeholder.com/150'),
+    (UUID(), 'Pieza 3', 'Descripción de la pieza 3', 39.99, 8, 'https://via.placeholder.com/150');
