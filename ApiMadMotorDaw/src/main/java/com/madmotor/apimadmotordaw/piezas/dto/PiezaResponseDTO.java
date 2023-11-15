f2f34b72e96a0145a34094ce25b901d297d02f7c
Ruben Fernandez <rubenfernandezp03@gmail.com>
2023-11-15 13:22:03 +0100

    Pieza coded and added constraints in pieza and his dtos

--- ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/piezas/dto/PiezaResponseDTO.java
+++ ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/piezas/dto/PiezaResponseDTO.java
@@ -1,4 +1,19 @@
 package com.madmotor.apimadmotordaw.piezas.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
 public class PiezaResponseDTO {
+    private UUID id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+    private String image;
+
+
 }
