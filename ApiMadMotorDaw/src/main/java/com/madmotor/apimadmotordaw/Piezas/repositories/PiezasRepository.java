5f4340ac23aa831fb7796fbc675031462327067e
Ruben Fernandez <rubenfernandezp03@gmail.com>
2023-11-15 13:17:05 +0100

    Pieza DTO coded and added queries to PiezaService

--- ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/Piezas/repositories/PiezasRepository.java
+++
@@ -1,6 +0,0 @@
-package com.madmotor.apimadmotordaw.Piezas.repositories;
-
-import com.madmotor.apimadmotordaw.Piezas.models.Pieza;
-
-public class PiezasRepository extends JpaRepository<Pieza, Long>, JpaSpecificationExecutor<Pieza>{
-}
---
+++ ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/piezas/dto/PiezaUpdateDTO.java
@@ -0,0 +1,5 @@
+package com.madmotor.apimadmotordaw.piezas.dto;
+
+public class PiezaUpdateDTO {
+
+}
