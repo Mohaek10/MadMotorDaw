98d98ef766cc55b34c9b64fac198c29e817f1875
Mohaek <elkasmimoha@gmail.com>
2023-11-19 14:55:26 +0100

    Arreglo de fallo del put de Categorias

--- ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/categorias/dto/CategoriaDto.java
+++ ApiMadMotorDaw/src/main/java/com/madmotor/apimadmotordaw/categorias/dto/CategoriaDto.java
@@ -1,13 +1,15 @@
 package com.madmotor.apimadmotordaw.categorias.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 public class CategoriaDto {
-    @NotBlank
+    @NotBlank(message = "El campo name no puede estar vacío")
     @Length(min=3, message = "El nombre debe tener al menos 3 caracteres")
     private final String name;
+    @NotNull(message = "El campo isDeleted no puede ser nulo")
     private final Boolean isDeleted;
 }
